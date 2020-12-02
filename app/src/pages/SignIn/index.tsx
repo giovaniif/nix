55a4d40afe489518c5e150812a7d89b2a047d95e
Giovani Farias <riccog.25@gmail.com>
2020-12-01 23:12:34 -0300

    :lipstick: Add SignIn Style
    Create SignIn Page Style

--- app/src/pages/SignIn/index.tsx
+++ app/src/pages/SignIn/index.tsx
@@ -1,16 +1,114 @@
-import React from 'react';
+import React, { useCallback, useState, useRef, useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Image, Keyboard, TextInput } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import * as Yup from 'yup';
 
-import Button from '../../components/Button';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
-import logoImage from '../../assets/logo.png';
+import Header from '../../components/Header';
+import Input from '../../components/Input';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import LogoImage from '../../assets/invertedLogo.png';
 
 import { 
-  Container, 
+  Container,
+  LogoContainer,
+  FormContainer,
+  Title,
+  Button,
+  ButtonText,
 } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
+  const { goBack, canGoBack } = useNavigation();
+  const formRef = useRef<FormHandles>(null);
+
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+
+  const handleSubmitForm = useCallback(async (data: SignInFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Digite um email válido!'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      // await api.post('users', data);
+
+      console.log('Cadastrado');
+
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        Keyboard.dismiss();
+
+        return;
+      }
+
+      console.log('Ocorreu um erro no cadastro');
+    }
+  }, []);
+
   return (
-    <Container />
+    <Container>
+      <Header onPress={() => canGoBack() && goBack()} />
+      <LogoContainer>
+        <Image 
+          source={LogoImage} 
+          style={{ height: 108, width: 108 }} 
+        />
+      </LogoContainer>
+      <FormContainer>
+        <Title>Faça seu login</Title>
+        <Form ref={formRef} onSubmit={handleSubmitForm}>
+          <Input 
+            ref={emailInputRef}
+            name="email"
+            icon="mail" 
+            placeholder="Seu melhor email" 
+            returnKeyType="next"
+            onSubmitEditing={() => {
+              passwordInputRef.current?.focus();
+            }}
+            />
+
+          <Input 
+            ref={passwordInputRef}
+            name="password" 
+            icon="lock" 
+            placeholder="Sua senha secreta" 
+            secureTextEntry
+            returnKeyType="send"
+            onSubmitEditing={() => formRef.current?.submitForm()}
+          />
+        </Form>
+        
+        <Button onPress={() => formRef.current?.submitForm()}>
+          <Feather name="log-in" color="#fafafa" size={24} />
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+      </FormContainer>
+    </Container>
   )
 }
 
--- app/src/pages/SignIn/styles.ts
+++ app/src/pages/SignIn/styles.ts
@@ -1,6 +1,57 @@
 import styled from 'styled-components/native';
+import Constants from 'expo-constants';
+
+const paddingTop = Constants.statusBarHeight + 52;
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #fafafa;
+  background-color: #2C5530;
+
+  padding-top: ${paddingTop}px;
+`;
+
+export const LogoContainer = styled.View`
+  flex: 2;
+  justify-content: center;
+  align-items: center;
+  background-color: #2C5530;
+`;
+
+export const FormContainer = styled.View`
+  flex: 4;
+  width: 100%;
+  background: #fafafa;
+  border-top-left-radius: 24px;
+  border-top-right-radius: 24px;
+
+  padding: 12px 18px;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-family: 'Poppins_600SemiBold';
+  font-size: 24px;
+  color: #263026;
+  
+  margin-bottom: 24px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  width: 100%;
+  height: 52px;
+  border-radius: 8px;
+  background-color: #348952;
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 24px;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 24px;
+  font-family: 'Poppins_400Regular';
+  color: #fafafa;
+
+  margin-left: 12px;
 `;
